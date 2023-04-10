7f0eec6d65f3f56f114c5fd805bf467008f4a7a9
jki12 <sjak0227@naver.com>
2023-04-10 17:29:12 +0900

    :)

--- src/App.tsx
+++ src/App.tsx
@@ -4,90 +4,103 @@ import './App.css';
 const SIZE = 3;
 
 function App() {
+  const tds = document.getElementsByTagName('td');
   const boardRef = useRef<number[]>([]);
-  const [isPlayerOneTurn, setIsPlayerOneTurn] = useState<boolean>();
-  let count = 0;
-
-  const decideTurn = () => {
-    return (Math.random() === 0);
-  }
+  const copyRef = useRef<number[]>([]);
+  const notationRef = useRef<number[]>([]); // 컴퓨터가 찾은 최선의 수..
+  const turnRef = useRef<number>(0);
 
   useEffect(() => {
     init();
   }, []);
 
   const init = () => {
-    setIsPlayerOneTurn(decideTurn());
+    turnRef.current = 0;
 
     for (let i = 0; i < SIZE * SIZE; ++i) {
-      boardRef.current[i] = 0;
+      boardRef.current[i] = -1;
+      copyRef.current[i] = -1;
     }
   }
 
-  const to2dIndex = (index : number) => {
-    const i = Math.floor(index / SIZE);
-    const j = Math.floor(index % SIZE);
-
-    console.log(i, j);
+  const isComputerTurn = () => {
+    return (turnRef.current % 2 === 0);
   }
 
   const place = (index : number) => {
     if (boardRef.current[index] !== 0) return;
 
-    boardRef.current[index] = (isPlayerOneTurn) ? 1 : 2;
-    setIsPlayerOneTurn(!isPlayerOneTurn); // toggle.
+    boardRef.current[index] = turnRef.current;
+    copyRef.current[index] = turnRef.current;
+    tds[index].innerText = (isComputerTurn() ? "X" : "O"); // change to image.
+
+    turnRef.current++;
+
+    console.log( boardRef.current );
   }
 
-  // lose : -1, draw : 0, win : 1
-  const determineGameResult = () => { // check return type.
+  /**
+   * 
+   * @returns point, lose : -1, draw : 0, win : 1
+   */
+  const determineGameResult = (board : number[]) => {
     const winnigCondition : number[][] = [
-      // col
+      // column
       [0, 1, 2], [3, 4, 5], [6, 7, 8],
       // row
       [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      //diag
+      //diagonal
       [0, 4, 8], [2, 4, 6]
     ];
 
     for (let i = 0; i < winnigCondition.length; ++i) {
-      if (boardRef.current[winnigCondition[i][0]] !== 1 && boardRef.current[winnigCondition[i][0]] === boardRef.current[winnigCondition[i][1]] && boardRef.current[winnigCondition[i][1]] === boardRef.current[winnigCondition[i][2]]) return 1;
-      else if (boardRef.current[winnigCondition[i][0]] !== 2 && boardRef.current[winnigCondition[i][0]] === boardRef.current[winnigCondition[i][1]] && boardRef.current[winnigCondition[i][1]] === boardRef.current[winnigCondition[i][2]]) return -1;
+      const [i1, i2, i3] = winnigCondition[i];
+
+      if (board[i1] !== 0 && board[i1] === board[i2] && board[i1] === board[i3]) {
+        return (board[i1] % 2 === 1) ? 1 : -1;
+      }
     }
 
     return 0;
   }
 
-  const tracking = (k: number, isMyTurn :boolean) : void => {
+  const tracking = (k: number) : void => {
     if (k === 9) {
-      count++;
+      if (determineGameResult(copyRef.current) === -1) { alert("something was wrong.."); return; }
 
+      // console.log(copyRef.current);
+      notationRef.current = copyRef.current.slice();
+      
       return;
     }
 
     for (let i = 0; i < SIZE * SIZE; ++i) {
-      if (boardRef.current[i] !== 0) continue;
+      if (copyRef.current[i] !== 0) continue;
       
-      boardRef.current[i] = (isMyTurn ? 1 : 2);
-      const gameResult = determineGameResult();
+      copyRef.current[i] = k;
+      const gameResult = determineGameResult(copyRef.current);
 
       if (gameResult === -1) { // 지는 수는 고려 안함.
-        boardRef.current[i] = 0;
+        copyRef.current[i] = 0;
 
-        return;
+        continue;
       }
-      else if (gameResult === 1) {
-        boardRef.current[i] = 0;
+      
+      tracking(k + 1);
 
-        // :)
-
-        return;
-      }
-
-      tracking(k + 1, !isMyTurn);
-      boardRef.current[i] = 0;
+      copyRef.current[i] = 0;
     }
+  }
 
-    if (k === 0) return;
+  const test = (index : number) => {
+    place(index);
+
+    // console.log(turnRef.current);
+    tracking(turnRef.current);
+
+    
+    console.log(notationRef.current);
+    console.log("find best way = ", notationRef.current[turnRef.current]);
   }
 
   return (
@@ -105,22 +118,21 @@ function App() {
       </thead>
       <tbody>
         <tr>
-            <td id="0" onClick={() => {place(0); console.log(boardRef.current[0]); console.log(determineGameResult())}}> 1 </td>
-            <td id="1" onClick={() => {place(1); console.log(boardRef.current[1]); console.log(determineGameResult())}}> 2 </td>
-            <td id="2" onClick={() => {place(2); console.log(boardRef.current[2]); console.log(determineGameResult())}}> 3 </td>
+            <td id="0" onClick={() => { test(0); }}> 0 </td>
+            <td id="1" onClick={() => { test(1); }}> 0 </td>
+            <td id="2" onClick={() => { test(2); }}> 0 </td>
         </tr>
         <tr>
-            <td id="3" onClick={() => {place(3); console.log(boardRef.current[3]); console.log(determineGameResult())}}> 4 </td>
-            <td id="4" onClick={() => {place(4); console.log(boardRef.current[4]); console.log(determineGameResult())}}> 5 </td>
-            <td id="5" onClick={() => {place(5); console.log(boardRef.current[5]); console.log(determineGameResult())}}> 6 </td>
+            <td id="3" onClick={() => { test(3); }}> 0 </td>
+            <td id="4" onClick={() => { test(4); }}> 0 </td>
+            <td id="5" onClick={() => { test(5); }}> 0 </td>
         </tr>
         <tr>
-            <td id="6" onClick={() => {place(6); console.log(boardRef.current[6]); console.log(determineGameResult())}}> 7 </td>
-            <td id="7" onClick={() => {place(7); console.log(boardRef.current[7]); console.log(determineGameResult())}}> 8 </td>
-            <td id="8" onClick={() => {place(8); console.log(boardRef.current[8]); console.log(determineGameResult())}}> 9 </td>
+            <td id="6" onClick={() => { test(6); }}> 0 </td>
+            <td id="7" onClick={() => { test(7); }}> 0 </td>
+            <td id="8" onClick={() => { test(8); }}> 0 </td>
         </tr>
         </tbody>
-        <button onClick={() => {tracking(0, true); console.log(count); count = 0; }}> b </button>
     </table>
 
   );
